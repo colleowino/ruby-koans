74a06a5ef5efea0e106b591a92dcb23990792467
colleowino <colleowino@gmail.com>
2015-07-05 18:03:39 +0300

    parallel assignment with splat operator

--- about_array_assignment.rb
+++ about_array_assignment.rb
@@ -21,7 +21,7 @@ class AboutArrayAssignment < Neo::Koan
   def test_parallel_assignments_with_splat_operator
     first_name, *last_name = ["John", "Smith", "III"]
     assert_equal "John", first_name
-    assert_equal __, last_name
+    assert_equal ["Smith", "III"], last_name
   end
 
   def test_parallel_assignments_with_too_few_variables
