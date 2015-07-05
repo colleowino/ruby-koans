8a429c977f7b7e45a5a52ad110bedaa0173c25d9
colleowino <colleowino@gmail.com>
2015-07-05 18:06:05 +0300

    parallel assignment with subarray

--- about_array_assignment.rb
+++ about_array_assignment.rb
@@ -32,8 +32,8 @@ class AboutArrayAssignment < Neo::Koan
 
   def test_parallel_assignments_with_subarrays
     first_name, last_name = [["Willie", "Rae"], "Johnson"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal ["Willie", "Rae"], first_name
+    assert_equal "Johnson", last_name
   end
 
   def test_parallel_assignment_with_one_variable
