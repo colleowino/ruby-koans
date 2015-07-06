dacabe469e8c566496d9fce676ce9c95d8a7cba9
colleowino <colleowino@gmail.com>
2015-07-06 08:32:54 +0300

    unordered hash are equal

--- about_hashes.rb
+++ about_hashes.rb
@@ -47,7 +47,7 @@ class AboutHashes < Neo::Koan
     hash1 = { :one => "uno", :two => "dos" }
     hash2 = { :two => "dos", :one => "uno" }
 
-    assert_equal __, hash1 == hash2
+    assert_equal true, hash1 == hash2
   end
 
   def test_hash_keys
