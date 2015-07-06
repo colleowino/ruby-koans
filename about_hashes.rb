8eed8e8569d74cffea3496ce61d2112d4d9bb4d5
colleowino <colleowino@gmail.com>
2015-07-06 08:03:54 +0300

    initialzing empty hash

--- about_hashes.rb
+++ about_hashes.rb
@@ -3,9 +3,9 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutHashes < Neo::Koan
   def test_creating_hashes
     empty_hash = Hash.new
-    assert_equal __, empty_hash.class
-    assert_equal(__, empty_hash)
-    assert_equal __, empty_hash.size
+    assert_equal Hash, empty_hash.class
+    assert_equal({} , empty_hash)
+    assert_equal 0, empty_hash.size
   end
 
   def test_hash_literals
