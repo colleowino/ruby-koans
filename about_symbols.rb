3c11987f3d43738e820c9d81e5e16fdaeda46e96
colleowino <colleowino@gmail.com>
2015-07-08 22:52:43 +0300

    symbols built using interpolation

--- about_symbols.rb
+++ about_symbols.rb
@@ -57,7 +57,7 @@ class AboutSymbols < Neo::Koan
     value = "and"
     symbol = :"cats #{value} dogs"
 
-    assert_equal __.to_sym, symbol
+    assert_equal "cats and dogs".to_sym, symbol
   end
 
   def test_to_s_is_called_on_interpolated_symbols
