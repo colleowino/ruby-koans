e9449cb805b38ae4094d1dbad18b23f6dff4a83d
colleowino <colleowino@gmail.com>
2015-07-09 23:54:38 +0300

    also capture matches

--- about_regular_expressions.rb
+++ about_regular_expressions.rb
@@ -127,9 +127,9 @@ class AboutRegularExpressions < Neo::Koan
   end
 
   def test_variables_can_also_be_used_to_access_captures
-    assert_equal __, "Name:  Gray, James"[/(\w+), (\w+)/]
-    assert_equal __, $1
-    assert_equal __, $2
+    assert_equal "Gray, James", "Name:  Gray, James"[/(\w+), (\w+)/]
+    assert_equal "Gray", $1
+    assert_equal "James", $2
   end
 
   # ------------------------------------------------------------------
