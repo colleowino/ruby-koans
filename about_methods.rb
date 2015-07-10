da6c226fdb206be96c6697bd6a5a694dc3682973
colleowino <colleowino@gmail.com>
2015-07-10 23:30:55 +0300

    testing calling private methods in other objects

--- about_methods.rb
+++ about_methods.rb
@@ -139,12 +139,12 @@ class AboutMethods < Neo::Koan
 
   def test_calling_methods_in_other_objects_require_explicit_receiver
     rover = Dog.new
-    assert_equal __, rover.name
+    assert_equal "Fido", rover.name
   end
 
   def test_calling_private_methods_in_other_objects
     rover = Dog.new
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       rover.tail
     end
   end
