ea7734cdd6ef0b9b379bc545ecc33f8b6f84f726
colleowino <colleowino@gmail.com>
2015-07-10 23:09:34 +0300

    missing paranthesis can sometimes be confusing

--- about_methods.rb
+++ about_methods.rb
@@ -11,14 +11,14 @@ class AboutMethods < Neo::Koan
   end
 
   def test_calling_global_methods_without_parentheses
-    result = my_global_method 2, 3
-    assert_equal __, result
+    result = my_global_method 2 , 3
+    assert_equal 5, result
   end
 
   # (NOTE: We are Using eval below because the example code is
   # considered to be syntactically invalid).
   def test_sometimes_missing_parentheses_are_ambiguous
-    eval "assert_equal 5, my_global_method 2, 3" # ENABLE CHECK
+    eval "assert_equal 5, (my_global_method 2, 3)" # ENABLE CHECK
     #
     # Ruby doesn't know if you mean:
     #
