4526260331d4e151b7d343043a94be04d4309fb3
colleowino <colleowino@gmail.com>
2015-07-10 23:24:13 +0300

    testing methods without explicit return

--- about_methods.rb
+++ about_methods.rb
@@ -88,7 +88,7 @@ class AboutMethods < Neo::Koan
   end
 
   def test_method_without_explicit_return
-    assert_equal __, method_without_explicit_return
+    assert_equal :return_value, method_without_explicit_return
   end
 
   # ------------------------------------------------------------------
