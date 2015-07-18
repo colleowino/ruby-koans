0c7a771cbae2f0c2afbfcb1450faa4b79f8ee0ea
colleowino <colleowino@gmail.com>
2015-07-18 22:53:58 +0300

    test next statement

--- about_control_statements.rb
+++ about_control_statements.rb
@@ -119,7 +119,7 @@ class AboutControlStatements < Neo::Koan
       next if (i % 2) == 0
       result << i
     end
-    assert_equal __, result
+    assert_equal  [1, 3, 5, 7, 9], result
   end
 
   def test_for_statement
