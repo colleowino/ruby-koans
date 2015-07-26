7ade97193a840fbf1552ea9fec192858adb8b887
colleowino <colleowino@gmail.com>
2015-07-26 23:12:27 +0300

    testing if block was passed

--- about_blocks.rb
+++ about_blocks.rb
@@ -54,8 +54,8 @@ class AboutBlocks < Neo::Koan
   end
 
   def test_methods_can_see_if_they_have_been_called_with_a_block
-    assert_equal __, yield_tester { :with_block }
-    assert_equal __, yield_tester
+    assert_equal :with_block, yield_tester { :with_block }
+    assert_equal :no_block, yield_tester
   end
 
   # ------------------------------------------------------------------
